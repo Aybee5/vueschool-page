893deae7e9c8b551fe4ae0b47039aaa4feffd790
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-30 23:02:14 +0100

    Remove styling from template

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="container mb-24">
+  <section class="container mb-24 lg:mb-14">
     <div class="flex flex-col lg:flex-row gap-12 md:gap-14 mt-8 items-center md:px-20 lg:px-0">
       <div class="flex flex-col gap-6 items-center lg:items-start">
-        <h1 class="font-bold text-[40px] text-white text-center lg:text-left">
+        <h1>
           Complete
-          <span class="text-brand-color">
+          <span>
             Vue.js training
           </span> solutions for companies
         </h1>
@@ -24,4 +24,12 @@
 
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  @apply font-bold text-[40px] text-white text-center lg:text-left;
+
+  & span {
+    @apply text-brand-color
+  }
+}
+</style>
