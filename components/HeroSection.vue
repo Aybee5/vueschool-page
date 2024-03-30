813b7b9888a242a420ad697361089fdfa0561135
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-30 13:29:03 +0100

    Creates hero sections

---
+++ components/HeroSection.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="container mb-24">
+    <div class="flex flex-col lg:flex-row gap-12 md:gap-14 mt-8 items-center md:px-20 lg:px-0">
+      <div class="flex flex-col gap-6 items-center lg:items-start">
+        <h1 class="font-bold text-[40px] text-white text-center lg:text-left">
+          Complete
+          <span class="text-brand-color">
+            Vue.js training
+          </span> solutions for companies
+        </h1>
+        <p class="text-light-color text-center lg:text-left">Training solutions designed for companies, agencies and
+          organisations
+          with
+          developers using or who are
+          considering using the Vue.js framework</p>
+        <BaseButton>Talk to Sales</BaseButton>
+      </div>
+      <img src="@/assets/images/hero-img.png" alt="Vue Solutions">
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
