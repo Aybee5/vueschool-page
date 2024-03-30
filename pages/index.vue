aa181ddb4a80a7599d317ccdc99c8e78a174c756
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-30 13:30:50 +0100

    Adds sections

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div class="svgContainer">
+      <HeroSection />
+      <AboutSection />
+      <SolutionsSection />
+    </div>
+    <BundleSection />
+    <WorkshopSection />
+  </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
