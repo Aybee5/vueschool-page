a5ec38b0c27addbb479c39ff206d0d25294453ef
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-30 13:31:10 +0100

    Adds section background svg

--- pages/index.vue
+++ pages/index.vue
@@ -13,3 +13,27 @@
 <script setup lang="ts">
 
 </script>
+
+<style scoped>
+.svgContainer {
+
+  background: url('@/assets/images/current-sm.svg') no-repeat;
+  background-position: bottom center;
+  background-blend-mode: overlay;
+  background-size: contain;
+
+
+  @media screen and (min-width: 767px) {
+    background: url('@/assets/images/current-md.svg') no-repeat;
+    background-position: bottom center;
+    background-size: contain;
+  }
+
+  @media screen and (min-width: 1200px) {
+    background: url('@/assets/images/current-lg.svg') no-repeat;
+    background-size: contain;
+    background-position: bottom center;
+  }
+
+}
+</style>
