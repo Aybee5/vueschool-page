05160ce98f9b823d3746ffd3dd7fa26a22d259a1
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-30 13:29:15 +0100

    Creates base button

---
+++ components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="px-10 py-5 bg-brand-color text-black rounded-[10px] font-medium">
+    <slot>
+      {{ text }}
+    </slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: String
+})
+</script>
+
+<style scoped></style>
