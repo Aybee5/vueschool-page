25ba410a8de4f538ffd1942bafd31360b811e534
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-31 11:44:23 +0100

    Remove unused head

--- app.vue
+++ app.vue
@@ -13,17 +13,4 @@ useSeoMeta({
   twitterDescription: 'Complete Vue.js training solutions for companies',
   twitterCard: 'summary'
 })
-
-useHead({
-  htmlAttrs: {
-    lang: 'en'
-  },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ]
-})
 </script>
