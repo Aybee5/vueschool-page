def32822f05c126efae221161dd3a603c3cf6045
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-31 09:10:28 +0100

    Setup sanity

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,16 @@
 export default defineNuxtConfig({
   css: ["@/assets/styles/main.css"],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
   googleFonts: {
     families: {
       Rubik: [400, 500, 700],
     },
+  },
+  modules: ["@nuxtjs/sanity", "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  sanity: {
+    projectId: process.env.NUXT_SANITY_PROJECT_ID,
+    dataset: process.env.NUXT_SANITY_DATASET,
+    useCdn: true,
+    apiVersion: process.env.NUXT_SANITY_API_VERSION || "2024-03-15",
   },
 });
