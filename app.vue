63638964cc04e8b86e132cfc17021f1778ec45c7
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-31 11:43:34 +0100

    Adds missing seo meta

--- app.vue
+++ app.vue
@@ -8,4 +8,22 @@
 
 <script setup>
 useSanity()
+useSeoMeta({
+  twitterTitle: 'VueSchool - Training Solutions',
+  twitterDescription: 'Complete Vue.js training solutions for companies',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
 </script>
