bd90537d719622e1f486b7aa3dcb7b33551b9656
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-31 11:19:54 +0100

    Adds nuxt seo package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,22 @@ export default defineNuxtConfig({
       Rubik: [400, 500, 700],
     },
   },
-  modules: ["@nuxtjs/sanity", "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxtjs/sanity",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/google-fonts",
+    "@nuxtjs/seo",
+  ],
   sanity: {
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
     dataset: process.env.NUXT_SANITY_DATASET,
     useCdn: true,
     apiVersion: process.env.NUXT_SANITY_API_VERSION || "2024-03-15",
+  },
+  site: {
+    url: "https://vueschool-page.netlify.app",
+    name: "VueSchool Page",
+    description: "Complete Vue.js training solutions for companies",
+    defaultLocale: "en",
   },
 });
