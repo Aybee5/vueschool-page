d2ef206e4faa679d5b60b675f0bc564ba5e8cd36
Ibrahim Abdullahi Aliyu <ibrahimabdul050@gmail.com>
2024-03-31 11:20:30 +0100

    Updates footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,130 +3,38 @@
     <div class="flex gap-12 md:gap-16 lg:gap-32 flex-col lg:flex-row lg:justify-between">
       <div class="flex items-center justify-center gap-8 flex-col lg:w-1/2 lg:justify-start lg:items-start">
         <img src="@/assets/images/logo.svg" alt="Vue School" class="w-[268px] h-[88px]">
-        <p class="text-white text-center">Thanks to fine people like you, Vue School can proudly sponsor Evan and the
+        <p class="text-white text-center lg:text-left">Thanks to fine people like you, Vue School can proudly sponsor
+          Evan and the
           future of
           Vue.js by
           being a
           Platinum Patreon</p>
         <span class="text-[#737B98] font-medium text-[22px]">FOLLOW US IN SOCIAL MEDIA</span>
         <div class="flex gap-3 md:gap-11 lg:gap-14">
-          <a href="https://www.patreon.com/vuejs" target="_blank">
+          <a href="#" target="_blank">
             <img src="@/assets/images/icons/social/facebook.svg" alt="Facebook">
           </a>
-          <a href="https://www.patreon.com/vuejs" target="_blank">
+          <a href="#" target="_blank">
             <img src="@/assets/images/icons/social/twitter.svg" alt="Twitter">
           </a>
-          <a href="https://www.patreon.com/vuejs" target="_blank">
+          <a href="#" target="_blank">
             <img src="@/assets/images/icons/social/github.svg" alt="GitHub">
           </a>
-          <a href="https://www.patreon.com/vuejs" target="_blank">
+          <a href="#" target="_blank">
             <img src="@/assets/images/icons/social/linkedin.svg" alt="Linkedin">
           </a>
-          <a href="https://www.patreon.com/vuejs" target="_blank">
+          <a href="#" target="_blank">
             <img src="@/assets/images/icons/social/youtube.svg" alt="Youtube">
           </a>
         </div>
       </div>
       <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-8 grow">
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">COMPANY</span>
+        <div v-for="(footer, index) in footerLinks" :key="index"
+          class="flex items-center md:items-start flex-col lg:self-center" :class="footer.class">
+          <span class="text-gray-header font-medium mb-5 text-[22px] uppercase">{{ footer.title }}</span>
           <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
-            </li>
-          </ul>
-        </div>
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">CORPORATE</span>
-          <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
-            </li>
-          </ul>
-        </div>
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">COMPANY</span>
-          <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
-            </li>
-          </ul>
-        </div>
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">COMPANY</span>
-          <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
-            </li>
-          </ul>
-        </div>
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">COMPANY</span>
-          <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
-            </li>
-          </ul>
-        </div>
-        <div class="flex items-center md:items-start flex-col">
-          <span class="text-gray-header font-medium mb-5 text-[22px]">COMPANY</span>
-          <ul class="flex flex-col gap-4 items-center md:items-start">
-            <li>
-              <a href="#" class="text-white">Courses</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Workshops</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Vue Masterclass</a>
-            </li>
-            <li>
-              <a href="#" class="text-white">Mastering Nuxt</a>
+            <li v-for="(link, index) in footer.links" :key="index">
+              <a :href="link.link" class="text-white">{{ link.text }}</a>
             </li>
           </ul>
         </div>
@@ -136,7 +44,122 @@
 </template>
 
 <script setup lang="ts">
+const footerLinks = [
+  {
+    title: 'Products',
+    links: [
+      {
+        text: 'Courses',
+        link: '#'
+      },
+      {
+        text: 'Workshops',
+        link: '#'
+      },
+      {
+        text: 'Vue Masterclass',
+        link: '#'
+      }
+      ,
+      {
+        text: 'Mastering Nuxt',
+        link: '#'
+      }
+    ],
+    class: "lg:order-1"
+  },
+  {
+    title: 'CORPORATE',
+    links: [
+      {
+        text: 'Corporate Training',
+        link: '#'
+      },
+      {
+        text: 'Developer Recruitment',
+        link: '#'
+      },
+      {
+        text: 'Course Development',
+        link: '#'
+      }
+      ,
+      {
+        text: 'On-site Workshops',
+        link: '#'
+      },
+      {
+        text: 'Consulting',
+        link: '#'
+      },
+      {
+        text: 'Affiliate Programs',
+        link: '#'
+      },
+    ],
+    class: "lg:order-4"
 
+  },
+  {
+    title: 'CONNECT WITH US',
+    links: [
+      {
+        text: 'About Us',
+        link: '#'
+      },
+      {
+        text: 'Careers',
+        link: '#'
+      },
+      {
+        text: 'Contact Us',
+        link: '#'
+      }
+    ],
+    class: "lg:order-5"
+  },
+  {
+    title: 'EVENTS',
+    links: [
+      {
+        text: 'Vue.js Nation',
+        link: '#'
+      },
+      {
+        text: 'Nuxt Nation',
+        link: '#'
+      },
+      {
+        text: 'Vue.js Forge',
+        link: '#'
+      }
+    ],
+    class: "lg:order-2"
+
+  },
+  {
+    title: 'RESOURCES',
+    links: [
+      {
+        text: 'Articles',
+        link: '#'
+      },
+    ],
+    class: "lg:order-6"
+  },
+  {
+    title: 'PRICING',
+    links: [
+      {
+        text: 'Plans',
+        link: '#'
+      },
+    ],
+    class: "lg:order-3"
+
+  },
+
+]
 </script>
 
 <style scoped></style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,8 +2,8 @@
   <nav class="bg-primary-color bg-opacity-90 border-gray-200">
     <div class="max-w-screen-2xl flex flex-wrap lg:flex-nowrap items-center justify-between mx-auto px-6 md:px-12 py-5">
       <div class="flex gap-6 items-center">
-        <button data-collapse-toggle="navbar-dropdown" type="button"
-          class="inline-flex items-center w-[26px] h-6 justify-center text-sm text-white rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+        <button @click="showMobileMenu = !showMobileMenu" data-collapse-toggle="navbar-dropdown" type="button"
+          class="inline-flex items-center w-[26px] h-6 justify-center text-sm text-white rounded-lg lg:hidden hover:opacity-70 focus:outline-none focus:ring-2 focus:ring-gray-200 "
           aria-controls="navbar-dropdown" aria-expanded="false">
           <span class="sr-only">Open main menu</span>
           <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -15,11 +15,12 @@
           <img src="@/assets/images/logo.svg" class="" alt="VueSchool Logo" />
         </a>
       </div>
-      <div class="hidden w-full lg:block md:w-auto absolute lg:relative lg:top-0 top-14" id="navbar-dropdown">
-        <ul class="flex flex-col p-4 md:p-0 mt-4 border gap-5 lg:flex-row lg:mt-0 lg:border-0 bg-primary-color">
+      <div class=" w-full lg:block order-1 lg:order-none lg:w-auto" id="navbar-dropdown"
+        :class="{ 'hidden': !showMobileMenu }">
+        <ul
+          class="font-medium flex flex-col p-4 lg:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 lg:flex-row gap-5 rtl:space-x-reverse lg:mt-0 lg:border-0 lg:bg-primary-color">
           <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-              class="flex items-center justify-between w-full nav-link">Courses
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="nav-link">Courses
               <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -30,29 +31,14 @@
             <a href="#" class="nav-link">Plans</a>
           </li>
           <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-              class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Business
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="nav-link">Business
               Services
               <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="m1 1 4 4 4-4" />
               </svg></button>
-            <!-- Dropdown menu -->
-            <div id="dropdownNavbar"
-              class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service 1</a>
-                </li>
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Service 2</a>
-                </li>
-              </ul>
 
-            </div>
           </li>
           <li>
             <a href="#" class="nav-link">Learning Paths</a>
@@ -64,37 +50,25 @@
             <a href="#" class="nav-link">Articles</a>
           </li>
           <li>
-            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
-              class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Events
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="nav-link">Events
               <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                   d="m1 1 4 4 4-4" />
               </svg></button>
-            <!-- Dropdown menu -->
-            <div id="dropdownNavbar"
-              class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                <li>
-                  <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Vue
-                    Launch</a>
-                </li>
-                <li>
-                  <a href="#"
-                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Nuxt
-                    Starter</a>
-                </li>
-              </ul>
 
-            </div>
           </li>
           <li>
             <a href="#" class="nav-link">Get Certified</a>
           </li>
+          <li class="items-center flex flex-col w-full gap-7 md:hidden">
+            <a href="#" class="nav-link text-center !block">Log in</a>
+            <a href="#" class="sign-up-link block text-center w-full">Sign up</a>
+          </li>
         </ul>
       </div>
       <div class="items-center gap-7 hidden md:flex">
-        <a href="#" class="text-white">Log in</a>
+        <a href="#" class="nav-link !text-light-color">Log in</a>
         <a href="#" class="sign-up-link">Sign up</a>
       </div>
     </div>
@@ -102,21 +76,26 @@
 </template>
 
 <script setup lang="ts">
-
+const showMobileMenu = ref(false)
 </script>
 
 <style scoped>
 .nav-link {
-  @apply py-2 px-3 text-light-color hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0;
+  @apply flex items-center justify-between w-full py-2 px-3 text-dark-color lg:text-light-color hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-brand-color md:p-0;
 }
 
 .sign-up-link {
+  @apply text-brand-color whitespace-nowrap;
   padding: 20px 40px;
   border-radius: 10px;
   border: 2px solid transparent;
-  background: linear-gradient(#212435, #212435) padding-box,
+  background: linear-gradient(white, white) padding-box,
     linear-gradient(to right, #0BD88F, #00C2FF) border-box;
-  @apply text-brand-color;
+
+  @media screen and (min-width: 767px) {
+    background: linear-gradient(#212435, #212435) padding-box,
+      linear-gradient(to right, #0BD88F, #00C2FF) border-box;
+  }
 
   &:hover {
     background: linear-gradient(#2B2F44, #2B2F44);
